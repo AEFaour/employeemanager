76918fd6d24edb3878c696c35f2c323745a2523a
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-09-01 15:58:49 +0200

    service add methods

--- server-side/src/main/java/hw/getarrays/serverside/service/EmployeeService.java
+++ server-side/src/main/java/hw/getarrays/serverside/service/EmployeeService.java
@@ -1,9 +1,37 @@
 package hw.getarrays.serverside.service;
 
+import hw.getarrays.serverside.model.Employee;
 import hw.getarrays.serverside.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EmployeeService {
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        System.out.println( "000" + UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+
+     public List<Employee> findAllEmployees() {
+        return  employeeRepository.findAll();
+     }
+
+     public Employee updateEmployee(Employee employee) {
+         return employeeRepository.save(employee);
+     }
+
+     public void deleteEmployee(Long id) {
+            employeeRepository.deleteById(id);
+     }
 }
